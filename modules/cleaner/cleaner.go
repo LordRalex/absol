64d7c065b63a695a338ca9540765594aa5776d89
Joshua Taylor <lordralex@gmail.com>
2020-03-09 14:50:18 -0500

    Fix compile

--- modules/cleaner/cleaner.go
+++ modules/cleaner/cleaner.go
@@ -40,70 +40,69 @@ func runTick(ds *discordgo.Session) {
 
 	cutOff := time.Now().Add(PostDelay)
 
-	for _, g := range ds.State.Guilds {
-		for _, channel := range channels {
-			c, err := ds.State.Channel(channel)
+	for _, channel := range channels {
+		c, err := ds.State.Channel(channel)
+		if err != nil {
+			// Try fetching via REST API
+			c, err = ds.Channel(channel)
 			if err != nil {
-				// Try fetching via REST API
-				c, err = ds.Channel(channel)
+				logger.Err().Printf("unable to fetch Channel for Message, %s", err)
+				return
+			} else {
+				// Attempt to add this channel into our State
+				err = ds.State.ChannelAdd(c)
 				if err != nil {
-					logger.Err().Printf("unable to fetch Channel for Message, %s", err)
+					logger.Err().Printf("error updating State with Channel, %s", err)
 					return
-				} else {
-					// Attempt to add this channel into our State
-					err = ds.State.ChannelAdd(c)
-					if err != nil {
-						logger.Err().Printf("error updating State with Channel, %s", err)
-						return
-					}
 				}
-			}
-
-			pinned, err := ds.ChannelMessagesPinned(c.ID)
-			if err != nil {
-				logger.Err().Printf("Error cleaning channel: %v", err.Error())
-				continue
-			}
-
-			messages := make([]string, 0)
-			//we use 1 here so that we go back to the beginning of time
-			chanMessages, err := ds.ChannelMessages(c.ID, 100, "", "1", "")
-			if err != nil {
-				logger.Err().Printf("Error cleaning channel: %v", err.Error())
-				continue
-			}
-
-			for _, m := range chanMessages {
-				skip := false
-				for _, p := range pinned {
-					if p.ID == m.ID {
-						skip = true
-					}
-				}
-
-				if skip {
-					continue
-				}
-
-				creationDate, err := m.Timestamp.Parse()
-				if err != nil {
-					continue
-				}
-
-				if creationDate.Before(cutOff) {
-					messages = append(messages, m.ID)
-					if len(messages) > 20 {
-						break
-					}
-				}
-			}
-
-			logger.Debug().Printf("Deleting %d messages", len(messages))
-
-			err = ds.ChannelMessagesBulkDelete(c.ID, messages)
-			if err != nil {
-				logger.Err().Printf("Error cleaning channel: %v", err.Error())
 			}
 		}
+
+		pinned, err := ds.ChannelMessagesPinned(c.ID)
+		if err != nil {
+			logger.Err().Printf("Error cleaning channel: %v", err.Error())
+			continue
+		}
+
+		messages := make([]string, 0)
+		//we use 1 here so that we go back to the beginning of time
+		chanMessages, err := ds.ChannelMessages(c.ID, 100, "", "1", "")
+		if err != nil {
+			logger.Err().Printf("Error cleaning channel: %v", err.Error())
+			continue
+		}
+
+		for _, m := range chanMessages {
+			skip := false
+			for _, p := range pinned {
+				if p.ID == m.ID {
+					skip = true
+				}
+			}
+
+			if skip {
+				continue
+			}
+
+			creationDate, err := m.Timestamp.Parse()
+			if err != nil {
+				continue
+			}
+
+			if creationDate.Before(cutOff) {
+				messages = append(messages, m.ID)
+				if len(messages) > 20 {
+					break
+				}
+			}
+		}
+
+		logger.Debug().Printf("Deleting %d messages", len(messages))
+
+		err = ds.ChannelMessagesBulkDelete(c.ID, messages)
+		if err != nil {
+			logger.Err().Printf("Error cleaning channel: %v", err.Error())
+		}
 	}
+
 }

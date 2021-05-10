1879daec4533685de0a18c8921ee4fc35a1330e8
Uriel Salischiker <uriel.salischiker@dynatrace.com>
2021-05-10 22:32:48 +0200

    New HJT implementation

--- modules/hjt/hjt.go
+++ modules/hjt/hjt.go
@@ -2,11 +2,13 @@ package hjt
 
 import (
 	"github.com/bwmarrin/discordgo"
+	"github.com/jinzhu/gorm"
 	"github.com/lordralex/absol/api"
-	"github.com/lordralex/absol/api/logger"
 	"github.com/lordralex/absol/api/database"
+	"github.com/lordralex/absol/api/logger"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -16,6 +18,7 @@ type Module struct {
 
 func (*Module) Load(ds *discordgo.Session) {
 	api.RegisterCommand("hjt", RunCommand)
+	api.RegisterCommand("hjtf", RunCommandFull)
 
 	api.RegisterIntentNeed(discordgo.IntentsGuildMessages, discordgo.IntentsDirectMessages)
 }
@@ -39,8 +42,7 @@ func RunCommand(ds *discordgo.Session, mc *discordgo.MessageCreate, cmd string, 
 		return
 	}
 
-	var values []string
-	err = db.Table("hjt").Where("? LIKE CONCAT('%', LOWER(name) ,'%')", content).Pluck("value", &values).Error
+	matches, err := getMatches(content, db)
 
 	if err != nil {
 		logger.Err().Printf("Failed to pull data from database\n%s", err)
@@ -48,11 +50,81 @@ func RunCommand(ds *discordgo.Session, mc *discordgo.MessageCreate, cmd string, 
 		return
 	}
 
-	if len(values) == 0 {
+	if len(matches) == 0 {
 		_, err = ds.ChannelMessageSend(mc.ChannelID, "No matches found")
 	} else {
-		_, err = ds.ChannelMessageSend(mc.ChannelID, strings.Join(values, ", "))
+		_, err = ds.ChannelMessageSend(mc.ChannelID, strings.Join(matches, ", "))
 	}
+}
+
+func RunCommandFull(ds *discordgo.Session, mc *discordgo.MessageCreate, cmd string, args []string) {
+	if len(args) == 0 {
+		return
+	}
+	pasteLink := args[0]
+
+	content, err := readFromUrlAsLowercase(pasteLink)
+	if err != nil {
+		_, err = ds.ChannelMessageSend(mc.ChannelID, "Invalid URL")
+		return
+	}
+
+	db, err := database.Get()
+	if err != nil {
+		_, err = ds.ChannelMessageSend(mc.ChannelID, "Failed to connect to database")
+		logger.Err().Printf("Failed to connect to database\n%s", err)
+		return
+	}
+
+	matches, err := getMatches(content, db)
+
+	if err != nil {
+		logger.Err().Printf("Failed to pull data from database\n%s", err)
+		_, err = ds.ChannelMessageSend(mc.ChannelID, "Failed to connect to database")
+		return
+	}
+
+	var objs []HJT
+	err = db.Table("hjt").Find(&objs, matches).Error
+	if err != nil {
+		_, err = ds.ChannelMessageSend(mc.ChannelID, "Failed to connect to database")
+		logger.Err().Printf("Failed to connect to database\n%s", err)
+		return
+	}
+
+	if len(objs) == 0 {
+		_, err = ds.ChannelMessageSend(mc.ChannelID, "No matches found")
+	} else {
+		var sb strings.Builder
+		sb.WriteString("Make sure to always double check the log manually before recommending any action\n")
+		for _, obj := range objs {
+			sb.WriteString(obj.severity + " ")
+			sb.WriteString(obj.category + " ")
+			sb.WriteString(obj.name + " ")
+			sb.WriteString(obj.value)
+			sb.WriteString("\n")
+		}
+		_, err = ds.ChannelMessageSend(mc.ChannelID, sb.String())
+	}
+}
+
+func getMatches(content string, db *gorm.DB) ([]string, error) {
+	var keys []string
+	err := db.Table("hjt").Pluck("name", &keys).Error
+	if err != nil {
+		return nil, err
+	}
+
+	var matches []string
+
+	for _, v := range keys {
+		matched, _ := regexp.MatchString(v, content)
+		if matched {
+			matches = append(matches, v)
+		}
+	}
+
+	return matches, nil
 }
 
 func readFromUrlAsLowercase(url string) (string, error) {
@@ -68,4 +140,11 @@ func readFromUrlAsLowercase(url string) (string, error) {
 		return "", err
 	}
 	return strings.ToLower(string(data)), nil
+}
+
+type HJT struct {
+	name     string `gorm:"primaryKey"`
+	value    string
+	category string
+	severity string
 }
